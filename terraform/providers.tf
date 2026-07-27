7933d477e76922a60f62a0e886673744fc83016e
AndreaScorza <atlantas95@gmail.com>
2026-07-27 09:00:45 +0200

    Added basic boilerplate

---
+++ terraform/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+  }
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
